dbde89644ddfad91f883cd9495e585c2d47d98c2
biseshadhikari <adhikaribisesh1590@gmail.com>
2025-05-10 12:58:53 +0545

    allowed host changed

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sh4du6x4v&cm#_p+q&gc2_^=l-9$p5@po09@xk=h&0f197v$5$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
